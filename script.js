5f0869160b1c1c03efed3f1572da79372d1298fe
Anna Rose Cimitile <annarosecimitile@gmail.com>
2025-07-09 14:59:45 -0400

    started JavaScript

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+songs = [
+    {
+        path: "audio/Touch The Sky (From BraveSoundtrack).mp3",
+        displayName: "Touch The Sky",
+        cover: "images/brave poster.png",
+        artist: "Julie Fowlis",
+    },
+    {
+        path: "audio/Hedwig's Theme.mp3",
+        displayName:"Hedwig's Theme",
+        cover: "images/harry potter poster.png",
+        artist: "John Williams",
+    },
+    {
+        path: "audio/Main Theme  Pirates of the Caribbean.mp3",
+        displayName:"He's a Pirate",
+        cover: "images/pirates poster.png",
+        artist: "Hans Zimmer",
+    },
+     {
+        path: "audio/Main Title.mp3",
+        displayName:"Main Title",
+        cover: "images/got poster.png",
+        artist: "Ramin Djawadi",
+    },
+];
